a0dd183f3bc4b0bc80ea73b304251ca278787219
silvia-hristova <60715761+silvia-hristova@users.noreply.github.com>
2020-04-05 17:42:17 +0300

    Add files via upload
    I changed the name of all methods of type bool to a question form.

--- chess.cpp
+++ chess.cpp
@@ -74,7 +74,7 @@ void Board::printBoard() {
 
 }
 
-bool Board::doMove() {
+bool Board::hasMoveBeenDone() {
 	using namespace std;
 	string move;
 	int x1, x2, y1, y2;
@@ -92,7 +92,7 @@ bool Board::doMove() {
 		{
 
 
-			if (makeMove(x1, y1, x2, y2) == false)
+			if (hasMoveBeenMade(x1, y1, x2, y2) == false)
 			{
 				cout << "Invalid move, try again." << endl;
 			}
@@ -166,15 +166,15 @@ void Board::setBoard()
 
 }
 
-bool Board::playGame()
+bool Board::hasGameBeenPlayed()
 {
 	system("cls");
 	printBoard();
-	return doMove();
+	return hasMoveBeenDone();
 
 }
 
-bool Board::moveKing(Square* thisKing, Square* thatSpace) {
+bool Board::hasKingBeenMoved(Square* thisKing, Square* thatSpace) {
 	//off board inputs should be handled elsewhere (before this)
 	//squares with same color should be handled elsewhere (before this)
 	if (abs(thatSpace->getX() - thisKing->getX()) == 1)
@@ -187,7 +187,7 @@ bool Board::moveKing(Square* thisKing, Square* thatSpace) {
 		else return false;
 	else return false;
 }
-bool Board::moveQueen(Square* thisQueen, Square* thatSpace) { //there might be problems with numbers of brackets
+bool Board::hasQueenBeenMoved(Square* thisQueen, Square* thatSpace) { //there might be problems with numbers of brackets
 													   //off board inputs should be handled elsewhere (before this)
 													   //squares with same color should be handled elsewhere (before this)
 
@@ -258,7 +258,7 @@ bool Board::moveQueen(Square* thisQueen, Square* thatSpace) { //there might be p
 	}
 }
 
-bool Board::moveBishop(Square* thisBishop, Square* thatSpace) { //there might be problems with number of brackets
+bool Board::hasBishopBeenMoved(Square* thisBishop, Square* thatSpace) { //there might be problems with number of brackets
 	int bishopX = thisBishop->getX();
 	int bishopY = thisBishop->getY();
 	int thatX = thatSpace->getX();
@@ -292,7 +292,7 @@ bool Board::moveBishop(Square* thisBishop, Square* thatSpace) { //there might be
 		return false;
 	}
 }
-bool Board::moveKnight(Square* thisKnight, Square* thatSpace)
+bool Board::hasKnightBeenMoved(Square* thisKnight, Square* thatSpace)
 {
 	//off board inputs should be handled elsewhere (before this)
 	//squares with same color should be handled elsewhere (before this)
@@ -313,7 +313,7 @@ bool Board::moveKnight(Square* thisKnight, Square* thatSpace)
 	}
 }
 
-bool Board::moveRook(Square* thisRook, Square* thatSpace)
+bool Board::hasRookBeenMoved(Square* thisRook, Square* thatSpace)
 {
 	//off board inputs should be handled elsewhere (before this)
 	//squares with same color should be handled elsewhere (before this)
@@ -363,7 +363,7 @@ bool Board::moveRook(Square* thisRook, Square* thatSpace)
 	}
 }
 
-bool Board::movePawn(Square* thisPawn, Square* thatSpace) {
+bool Board::hasPawnBeenMoved(Square* thisPawn, Square* thatSpace) {
 	//off board inputs should be handled elsewhere (before this)
 	//squares with same color should be handled elsewhere (before this)
 	using namespace std;
@@ -415,7 +415,7 @@ bool Board::movePawn(Square* thisPawn, Square* thatSpace) {
 		else
 			return false;
 }
-bool Board::makeMove(int x1, int y1, int x2, int y2) {
+bool Board::hasMoveBeenMade(int x1, int y1, int x2, int y2) {
 	//Checking for turns will be done previous to this
 	using namespace std;
 	if (x1 < 0 || x1>7 || y1 < 0 || y1>7 || x2 < 0 || x2>7 || y2 < 0 || y2>8)
@@ -434,21 +434,21 @@ bool Board::makeMove(int x1, int y1, int x2, int y2) {
 
 	switch (src->getPiece())
 	{
-	case KING: return moveKing(src, dest);
+	case KING: return hasKingBeenMoved(src, dest);
 		break;
-	case QUEEN: return moveQueen(src, dest);
+	case QUEEN: return hasQueenBeenMoved(src, dest);
 		break;
-	case BISHOP: return moveBishop(src, dest);
+	case BISHOP: return hasBishopBeenMoved(src, dest);
 		break;
-	case KNIGHT: return moveKnight(src, dest);
+	case KNIGHT: return hasKnightBeenMoved(src, dest);
 		break;
-	case ROOK: return moveRook(src, dest);
+	case ROOK: return hasRookBeenMoved(src, dest);
 		break;
-	case PAWN: return movePawn(src, dest);
+	case PAWN: return hasPawnBeenMoved(src, dest);
 		break;
 	case EMPTY: std::cout << "You do not have a piece there" << std::endl;  return false;
 		break;
-	default: std::cerr << "Something went wrong in the switch statement in makeMove()" << std::endl;
+	default: std::cerr << "Something went wrong in the switch statement in hasMoveBeenMade()" << std::endl;
 		break;
 	}
 	return false;
